7c2be2d5c77938ebd3f95c99dc418183e37ff085
Charles Morgan <chmorgan85@gmail.com>
2023-08-28 12:08:31 -0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,47 @@
+export const projects = [
+  {
+    title: "SocialNews",
+    subtitle: "ASP.NET Core",
+    description:
+      "A full-stack web application for sharing news articles, featuring a voting system, commenting, user authentication/authorization and more!",
+    image: "./SocialNews.png",
+    link: "https://github.com/chmorgan85/SocialNews",
+  },
+  {
+    title: "JobFinder",
+    subtitle: "ASP.NET Core",
+    description:
+      "A full-stack web application that helps you find and keep track of job listings.",
+    image: "./JobFinder.png",
+    link: "https://github.com/chmorgan85/JobFinder",
+  },
+  {
+    title: "Notes App",
+    subtitle: "Flask",
+    description:
+      "A full-stack web application for taking notes.",
+    image: "./NotesApp.png",
+    link: "https://github.com/chmorgan85/notes-app",
+  },
+  {
+    title: "Portfolio",
+    subtitle: "React",
+    description:
+      "My professional portfolio.",
+    image: "./Portfolio.png",
+    link: "/#",
+  },
+];
+
+export const skills = [
+  "C#",
+  "Python",
+  "JavaScript",
+  "SQL",
+  "ASP.NET Core",
+  "Entity Framework Core",
+  "React",
+  "HTML",
+  "CSS",
+  "Bootstrap",
+];
